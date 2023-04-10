cb26bd328affafa1ccab83f8aa43b4a85566d13b
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2023-04-11 03:52:46 +0530

    Cleanup.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,9 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+})
+const openai = new OpenAIApi(configuration)
 
 const conversation = []
 
@@ -11,20 +11,20 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
+        message: 'OpenAI API key not configured, please follow instructions in README.md',
       }
-    });
-    return;
+    })
+    return
   }
 
-  const content = req.body.input || '';
+  const content = req.body.input || ''
   if (content.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid message",
+        message: 'Please enter a valid message',
       }
-    });
-    return;
+    })
+    return
   }
 
   try {
@@ -32,7 +32,7 @@ export default async function (req, res) {
     if (isUserBanned()) {
       res.status(400).json({
         error: {
-          message: "You are not allowed to use this API",
+          message: 'You are not allowed to use this API',
         }
       })
       return
@@ -42,33 +42,33 @@ export default async function (req, res) {
     updateConversation({content, isBot: false})
     
     const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: 'gpt-3.5-turbo',
       // max_tokens: 100,
       messages: [
         ...conversation,
         { role: 'user', content }
       ],
       temperature: 0,
-    });
+    })
     const result = completion.data.choices[0].message.content
 
     // add message to conversation
     updateConversation({content: result, isBot: true})
 
     // return the result
-    res.status(200).json({ result, full: completion.data.choices });
+    res.status(200).json({ result, full: completion.data.choices })
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json(error.response.data)
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
+      console.error(`Error with OpenAI API request: ${error.message}`)
       res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
         }
-      });
+      })
     }
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,33 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from 'next/head'
+import { useState } from 'react'
+import styles from './index.module.css'
 
 export default function Home() {
-  const [inputMessage, setInputMessage] = useState("");
-  const [result, setResult] = useState();
+  const [inputMessage, setInputMessage] = useState('')
+  const [result, setResult] = useState()
 
   async function onSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
+      const response = await fetch('/api/generate', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({ input: inputMessage }),
-      });
+      })
 
-      const data = await response.json();
+      const data = await response.json()
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw data.error || new Error(`Request failed with status ${response.status}`)
       }
 
-      setResult(data.result);
-      setInputMessage("");
+      setResult(data.result)
+      setInputMessage('')
     } catch(error) {
       // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+      console.error(error)
+      alert(error.message)
     }
   }
 
@@ -35,24 +35,24 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <link rel='icon' href='/dog.png' />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
+        <img src='/dog.png' className={styles.icon} />
         <h3>OptimusGPT</h3>
         <form onSubmit={onSubmit}>
           <input
-            type="text"
-            name="message"
-            placeholder="Type a message and press <enter>"
+            type='text'
+            name='message'
+            placeholder='Type a message and press <enter>'
             value={inputMessage}
             onChange={(e) => setInputMessage(e.target.value)}
           />
-          <input type="submit" value="Send message" />
+          <input type='submit' value='Send message' />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
     </div>
-  );
+  )
 }
